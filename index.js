d5654d8d092e3982768d7a5b8458c34d4f8a5288
Czer0C <trieunguyen04239@gmail.com>
2025-01-06 10:35:33 +0700

    port

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 require('dotenv').config()
 
+const PORT = process.env.SERVICE_PORT || 5000
+
+
 const app = express()
 
 const cors = require('cors')
@@ -12,12 +15,11 @@ app.use(express.json())
 // for JSON bodies
 app.use(express.urlencoded({ extended: true })) // for URL-encoded bodies
 
-const PORT = process.env.SERVICE_PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Alive')
 })
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ' + process.env.SERVICE_PORT)
+    console.log('Server is running on port ' + PORT)
 })
