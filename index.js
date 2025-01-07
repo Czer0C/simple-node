b4187c23bf08c2fd22f1ff9e7434a7dfe5f0845a
Czer0C <trieunguyen04239@gmail.com>
2025-01-07 09:54:16 +0700

    lol

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require('express')
+const express = require('express');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.SERVICE_PORT || 5000
+const PORT = process.env.SERVICE_PORT || 5000;
 
-const app = express()
+const app = express();
 
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 // for JSON bodies
-app.use(express.urlencoded({ extended: true })) // for URL-encoded bodies
+app.use(express.urlencoded({ extended: true })); // for URL-encoded bodies
 
+const list = ['You smart', 'You genius', 'You loyal', 'I Appreciate you'];
 
 app.get('/', (req, res) => {
-    res.send('You smart')
-})
+  const rand = Math.floor(Math.random() * 100) % 4;
+
+  res.send(`${list[rand]}`);
+});
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT)
-})
+  console.log('Server is running on port ' + PORT);
+});
